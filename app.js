11b3997ec21d2a3191af905b9aa0785e15162631
Dedesyarifudin07 <dedesyarifudinalhadid@gmail.com>
2022-09-14 22:10:26 +0700

    mengganti package monggoose jadi monggodb

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//mongose
+const { MongoClient } = require('mongodb');
+
+
 //routes
 app.get('/',(req,res) => {
     res.send('ini adalah halaman home')
 })
+
+
+//connect to db
+mongoose.connect('mongodb://localhost:27017/pelajar')
+ const db = mongoose.connection
+
 
 
 //'listen 
