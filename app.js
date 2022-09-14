c5a0243c08ed112c549db0f7305c6a6bb05cfbf9
Dedesyarifudin07 <dedesyarifudinalhadid@gmail.com>
2022-09-14 22:46:19 +0700

    memodifikasi file app.js

--- app.js
+++ app.js
@@ -13,12 +13,71 @@ app.get('/',(req,res) => {
 
 
 //connect to db
-mongoose.connect('mongodb://localhost:27017/pelajar')
- const db = mongoose.connection
+const uri = 'mongodb://127.0.0.1/27017';
+const dbName = 'Pelajar';
+const client = new MongoClient(uri,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
 
+client.connect((error,client) => {
+    if(error){
+        return console.log('koneksi gagal');
+    }
 
+    console.log('onneksi berhasil')
+})  
 
 //'listen 
 app.listen(port,(req,res) => {
     console.log(`server telah berjalan di port ${port}`);
 })
+
+
+//pilih database
+const db = client.db(dbName)
+
+
+//menambahkan data ke db ccolection pelajar SATU PERSATU
+// db.collection('mahasiswa').insertOne(
+//     {
+//         nama:"erik",
+//         email:"erik@gmail.com"
+//     },
+//     (error,result) => {
+//     if(error){
+//         console.log('data gagal ditambahkan')
+//     }
+
+//     console.log(result)
+
+//    }
+// )
+
+
+//menambahkan data ke db dengan cara langsung banyak
+
+db.collection('mahasiswa').insertMany(
+    [
+        {
+            nama:"Dede syarifudin",
+            email:"alhadid@gmail.com"
+        },
+        {
+            nama:"ahmad syarifudin",
+            email:"ahmad@gmail.com"
+        },
+        {
+            nama:"ipul gunawan",
+            email:"ipul@gmail.com"
+        }
+    ],
+
+    (error,result) => {
+        if(error){
+            console.log('data gagal ditambahkan ke data base')
+        }
+
+        console.log(result);
+    }
+)
